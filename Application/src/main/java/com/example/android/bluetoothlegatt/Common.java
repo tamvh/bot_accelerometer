cac7b9297ec71435404e31dad660f29087c762cd
tamvh <huytamuit@gmail.com>
2017-04-14 16:30:32 +0700

    init project

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/Common.java
@@ -0,0 +1,10 @@
+package com.example.android.bluetoothlegatt;
+
+/**
+ * Created by tamvh on 04/10/2016.
+ */
+public class Common {
+    public static String ServiceId = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+    public static String writeCharacteristicId02 = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"; //control 1next, 2back, 3left, 4right, 0stop
+    public static String writeCharacteristicId04 = "6e400004-b5a3-f393-e0a9-e50e24dcca9e"; //pwm 0- 255
+}
